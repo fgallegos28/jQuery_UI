765bc6ce22e6213a3a4b308c5ee0534471b7e9be
Francisco <fgallegos@unitedvirtualities.com>
2020-01-30 18:50:09 -0700

    all the exercises finished

---
+++ jqueryui-effects-exercise-files/before/scripts/page.js
@@ -0,0 +1,55 @@
+﻿/// <reference path="jquery-1.6.2.js" />
+/// <reference path="jquery-ui-1.8.16.custom.js" />
+
+
+//add Colors
+$(function () {
+    $("#target").click(function(){
+        $(this).animate({
+            "width": "+=20",
+            "height": "+=20",
+            "background-color": "red"
+        });
+    });
+});
+
+//add Effects - visit page for more effects https://jqueryui.com/effect/
+$(function () {
+   /* $("#target").click(function(){
+        $(this).effect("explode",
+                {pieces: 16}, "slow", function () {
+                    alert("done!");
+                });
+    });*/
+
+    $("#draggables").children().draggable();
+
+    $("#trash").droppable({
+        activeClass: "opaque",
+        drop: function (event, ui) {
+            ui.draggable.hide("explode");
+        }
+    });
+});    
+    /*
+    //Easing
+    $(function () {
+        $("#target")
+            .css("position", "relative")
+            .click(function() {
+               $(this).animate({
+                   left: 300
+               }, 1000, "easeInOutElastic"); 
+            });
+
+});*/
+
+    //Easing
+    $(function () {
+        $("#target")
+            .css("position", "relative")
+            .click(function () {
+                $(this).addClass("leftTarget");
+            });
+
+    });
---
+++ jqueryui-interactions-exercise-files/before/scripts/page.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="jquery-1.6.2.js" />
+/// <reference path="jquery-ui-1.8.16.custom.js" />
+
+
+
+$(function () {
+    //$("#draggables").children().draggable();
+    
+    $("#d1").draggable({ 
+        //axis: "x" //move to eje x
+        revert: "invalid" //regresa a su lugar de incio
+    }); 
+
+    $("#d2").draggable({ 
+        //axis: "y" //move to eje y
+        snap: "#d1, #d3", //permite desplazarse alrededor de d1 y d3
+        helper: function() {
+            return $("<div>I am a helper</div>");
+        }
+    });
+
+    $("#d3").draggable({ 
+        //se puede desplazar por todas partes
+        contaiment: "#draggables",
+        handle: "header"
+    }); 
+
+
+    $("#d1, #d2, #d3").draggable(
+        "option", "stack", ".ui-draggable"
+    );
+
+    //drop the header's
+    $("#trash").droppable({
+        activeClass: "opaque",
+        accept: "#d3",
+        drop: function (event, ui){
+            ui.draggable.fadeOut(function (){
+                ui.draggable.remove();
+            });
+        }
+    });
+
+    //List "sortable"
+    //axis: para mover "li" en este caso sobre el eje "y"
+    $("#sortable").sortable({ axis: "y", placeholder: "placeholder" });
+
+
+    //resizable: permite redimensionar el ancho y alto
+    $("#d3").resizable({ aspectRatio: true, alsoResize: "#d2" });
+});
+
